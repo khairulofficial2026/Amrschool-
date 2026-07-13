c705084fcfa91113d04dcaf55b4863690c606955
khairulofficial2026 <khairulofficial92@gmail.com>
2026-07-13 22:46:48 +0600

    Change SHEET_CSV_URL to generated file tag
    Updated SHEET_CSV_URL to use a generated file tag.

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // এখানে আপনার Google Sheet এর "Publish to web" CSV লিংক বসান
 // কীভাবে বের করবেন তার নিয়ম README.md ফাইলে দেওয়া আছে
 // ================================
-const SHEET_CSV_URL = "PASTE_YOUR_PUBLISHED_CSV_LINK_HERE";
+const SHEET_CSV_URL = "[file-tag: code-generated-file-1783961139418939766]";
