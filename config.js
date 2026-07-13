cfed5d427e0e656cf8ba98a0d27c17fecb876906
khairulofficial2026 <khairulofficial92@gmail.com>
2026-07-13 22:57:04 +0600

    Update Google Sheet CSV URL in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // এখানে আপনার Google Sheet এর "Publish to web" CSV লিংক বসান
 // কীভাবে বের করবেন তার নিয়ম README.md ফাইলে দেওয়া আছে
 // ================================
-const SHEET_CSV_URL = "[file-tag: code-generated-file-1783961139418939766]";
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRWXfmhb480HkYvS3CWWnPnDgTTKKTCGLBAAPsJgu4zXJ_xdESXLvQYNsrY6cmRg/pub?gid=1161613645&single=true&output=csv";
